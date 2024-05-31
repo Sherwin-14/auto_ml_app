35b6b9e3eef2a0a9a9cbe7b94d83b147bfe3d959
Sherwin-14 <varghesesheriwn@gmail.com>
2024-05-31 06:49:35 +0000

    Application is genralized

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ import pandas as pd
 import os
 from ydata_profiling import ProfileReport
 from streamlit_ydata_profiling import st_profile_report
+from pygwalker.api.streamlit import StreamlitRenderer
 
 #ML Stuff
-from pycaret.classification import setup, compare_models, pull, save_model
+from pycaret.classification import setup as setup_classification, compare_models as compare_models_classification, pull, save_model
+from pycaret.regression import setup as setup_regression, compare_models as compare_models_regression
+
+st.set_page_config(
+        page_title="Use Pygwalker In Streamlit",
+        layout="wide"
+    )
 
 with st.sidebar:
     #st.image()
     st.title("AutoStreamML")
-    choice = st.radio("Navigation",["Upload","Profiling","ML","Download"])
+    choice = st.radio("Navigation",["Upload","Profiling","ML","Download","Visualizer"])
     st.info("This application allows you to build an automated ML pipeline using Streamlit, Pandas Profiling, PyCaret")
 
 if os.path.exists("source_data.csv"):
@@ -37,16 +44,30 @@ if choice == "Profiling":
 if choice == "ML":
     st.title("Train your model")
     target = st.selectbox("Select your target feature",df.columns)
-    setup(df, target=target)
-    setup_df= pull()
-    st.info("This is an ML Experiment Settings")
-    st.dataframe(setup_df)
-    best_model= compare_models()
-    compare_df = pull()
-    st.info("This is the Ml Model")
-    st.dataframe(compare_df)
-    best_model
+    problem_type = st.selectbox("Select your problem type", ["Classification", "Regression"])
+    if st.button("Train Model"):
+        if problem_type == "Classification":
+            setup_classification(df, target=target)
+            best_model= compare_models_classification()
+        elif problem_type == "Regression":
+            setup_regression(df, target=target)
+            best_model= compare_models_regression()
+
+        compare_df = pull()
+        st.info("This is the ML Model")
+        st.dataframe(compare_df,use_container_width= True)
+        best_model
+        save_model(best_model,'best_model')
 
 
 if choice == "Download":
-    pass
+    st.title("Hurray! You can now download your model")
+    with open("best_model.pkl","rb") as f:
+        st.download_button("Download the Model",f,"best_model.pkl")
+
+if choice == "Visualizer":
+
+    st.title("Visualize Your Data")
+    pyg_app = StreamlitRenderer(df)
+    
+    pyg_app.explorer()
